20c2716fb2586068b6f5190d041411c713f599ce
Svetlana R <svetlana.rovinsky@gmail.com>
2019-05-23 12:06:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function dwarfRollCall(dwarves) {
+let dwarvesArray= dwarves;
+let numberedArray=[];
+  for ( let i=0; i<dwarvesArray.length; i++){
+    numberedArray +=`${i+1}. ${dwarvesArray[i]} `
+  }
+  return numberedArray;
 }
 
 function summonCaptainPlanet(planeteerCalls){

caa8d64fa0af583ce402a170f1b5b1e4626b81cf
Svetlana R <svetlana.rovinsky@gmail.com>
2019-06-03 00:12:41 +0000

    Done.

--- index.js
+++ index.js
@@ -8,10 +8,33 @@ let numberedArray=[];
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  let capitalArray= [];
+  for ( let i=0; i< planeteerCalls.length; i++){
+    let eachCall =
+    capitalArray.push(`${planeteerCalls[i]}!`.toUpperCase());
+  }
+  return capitalArray;
 }
 
 function longPlaneteerCalls(words) {
+  for ( let i=0; i< words.length; i++){
+    let eachWord= words[i];
+    if (eachWord.length >4){
+      return true
+    }
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
+  let cheeseArray= ["cheddar", "gouda", "camembert"];
+  for ( let i=0; i<foods.length; i++){
+    let foodItem= foods[i];
+    for ( let j=0; j< cheeseArray.length; j++){
+      if ( cheeseArray[j] === foodItem){
+        return foodItem;
+      }
+    }
+  }
+  return "no cheese!"
 }
